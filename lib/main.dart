eb63199f829d65c8f2825d18092d0001791f91d1
HebaMahdy <hebamahdy.86@gmail.com>
2023-09-27 15:44:04 +0300

    create notes app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const NotesApp());
 }
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
