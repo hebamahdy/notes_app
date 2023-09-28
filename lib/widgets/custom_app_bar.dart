5c3ab4f6b4f7a58e367d04d44cbbb2f93318c8e8
HebaMahdy <hebamahdy.86@gmail.com>
2023-09-28 08:27:24 +0300

    Custom App bar

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:notes/widgets/custom_icon_dart.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+
+        Text("Notes",style: TextStyle(fontSize: 28),),
+        Spacer(),
+        CustomSearchIcon()
+      ],
+
+    );
+  }
+}
+
